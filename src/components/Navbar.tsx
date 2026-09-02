88c1e71a5585cb621ba7a68f7dc1689794fdabf7
Viktor Veytsman <veytsman1963@gmail.com>
2026-09-02 15:31:50 -0400

    Added Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">Niviol</div>
+      <ul className="navbar-links">
+        <li>
+          <NavLink to="/" end className={({ isActive }) => (isActive ? 'active' : '')}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/listings" className={({ isActive }) => (isActive ? 'active' : '')}>
+            Listings
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" className={({ isActive }) => (isActive ? 'active' : '')}>
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact" className={({ isActive }) => (isActive ? 'active' : '')}>
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
